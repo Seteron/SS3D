69fb9aa7735ce43145d4bcb8d403c55230e4f656
Seteron <38957910+Seteron@users.noreply.github.com>
2020-05-28 20:29:48 -0500

    Basic throwing
    Adds underhanded and overhanded throwing, adds some anim files and modifies the player animator controller slightly.

--- Assets/Engine/Inventory/Extensions/Hands.cs
+++ Assets/Engine/Inventory/Extensions/Hands.cs
@@ -22,6 +22,8 @@ namespace SS3D.Engine.Inventory.Extensions
         public GameObject ContainerObject => Container.gameObject;
         public int HeldSlot => handSlots[SelectedHand];
 
+        public Vector3 mouseTarget;
+
         public void Start()
         {
             SupportsMultipleInteractions = true;
@@ -48,8 +50,12 @@ namespace SS3D.Engine.Inventory.Extensions
         {
             if (GetItemInHand() == null) return;
 
-            var transform = GetItemInHand().transform;
-            inventory.PlaceItem(ContainerObject, HeldSlot, transform.position);
+            var heldItem = GetItemInHand();
+            var storePosition = heldItem.transform.position;
+            var storeRotation = heldItem.transform.rotation;
+            inventory.PlaceItem(ContainerObject, HeldSlot, storePosition);
+            heldItem.transform.position = storePosition;
+            heldItem.transform.rotation = storeRotation;
         }
         [Server]
         public void PlaceHeldItem(Vector3 position) => inventory.PlaceItem(ContainerObject, HeldSlot, position);
@@ -132,6 +138,8 @@ namespace SS3D.Engine.Inventory.Extensions
             }
 
             if (Input.GetButtonDown("Drop Item")) CmdDropHeldItem();
+            if (Input.GetButtonDown("Throw")) StartThrow();
+
         }
 
         [Command]
@@ -153,6 +161,101 @@ namespace SS3D.Engine.Inventory.Extensions
             }
         }
 
+        private void SetTarget()
+        {
+            Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out var hit);
+            mouseTarget = hit.point;
+            CmdSetTarget(mouseTarget);
+        }
+        [Command]
+        private void CmdSetTarget(Vector3 _mouseTarget)
+        {
+            mouseTarget = _mouseTarget;
+        }
+
+        private void StartThrow()
+        {
+            if (GetItemInHand() == null) return;
+            GameObject heldItem = GetItemInHand().gameObject;
+
+            SetTarget();
+
+            Vector3 lookRotation = mouseTarget - heldItem.transform.position;
+            lookRotation.y = 0;
+            transform.root.rotation = Quaternion.LookRotation(lookRotation, Vector3.up);
+
+            Animator playerAnimator = GetComponentInParent<Animator>();
+            playerAnimator.SetLayerWeight(1, 1);
+            playerAnimator.SetInteger("ActiveHand",SelectedHand);
+
+            if(Input.GetButton("Harm")) StartThrowOver(heldItem, playerAnimator);
+            else StartThrowUnder(heldItem, playerAnimator);
+        }
+
+        private void StartThrowUnder(GameObject heldItem, Animator playerAnimator)
+        {
+            float throwWeight = (mouseTarget - heldItem.transform.position).magnitude * 0.3f;
+            throwWeight = Mathf.Clamp(throwWeight, 0.15f, 1f);
+            playerAnimator.SetLayerWeight(1, throwWeight);
+            playerAnimator.SetTrigger("ThrowUnder");
+        }
+
+        private void StartThrowOver(GameObject heldItem, Animator playerAnimator)
+        {
+            float throwWeight = (mouseTarget - heldItem.transform.position).magnitude * 0.3f;
+            throwWeight = Mathf.Clamp(throwWeight, 0.3f, 0.7f);
+            playerAnimator.SetLayerWeight(1, throwWeight);
+            playerAnimator.SetTrigger("ThrowOver");
+        }
+
+        [Server]
+        private void DoThrowUnder()
+        {
+            if (GetItemInHand() == null) return;
+            GameObject heldItem = GetItemInHand().gameObject;
+            DropHeldItem();
+
+            Vector3 targetPosition = mouseTarget - heldItem.transform.position;
+            targetPosition = Vector3.ClampMagnitude(targetPosition, 4);
+
+            Rigidbody rigid = heldItem.GetComponent<Rigidbody>();
+            rigid.AddForce(CannonAngle(heldItem.transform.position, targetPosition) * rigid.mass, ForceMode.Impulse);
+            rigid.AddTorque(new Vector3(3, 3, 3), ForceMode.Impulse);
+        }
+
+        [Server]
+        private void DoThrowOver()
+        {
+            if (GetItemInHand() == null) return;
+            GameObject heldItem = GetItemInHand().gameObject;
+            DropHeldItem();
+            Vector3 aimVector = mouseTarget - heldItem.transform.position;
+            aimVector.y = 0f;
+            Vector3 launchVector = aimVector.normalized * 12 + Vector3.up * 2;
+            Rigidbody rigid = heldItem.GetComponent<Rigidbody>();
+            rigid.AddForce(launchVector, ForceMode.Impulse);
+            Quaternion torque = Quaternion.FromToRotation(Vector3.forward, aimVector.normalized);
+            heldItem.transform.rotation = Quaternion.FromToRotation(Vector3.forward, aimVector.normalized);
+            rigid.maxAngularVelocity = 15;
+            rigid.angularVelocity = torque * Vector3.right * 15;
+  //          Time.timeScale = 0.2f;
+        }
+
+        public Vector3 CannonAngle(Vector3 sourcePosition, Vector3 targetPosition)
+        {
+    
+            Vector3 dir = targetPosition; // get Target Direction
+            float height = dir.y; // get height difference
+            dir.y = 0; // retain only the horizontal difference
+            float dist = dir.magnitude; // get horizontal direction
+            float a = 45 * Mathf.Deg2Rad; // Convert angle to radians
+            dir.y = dist * Mathf.Tan(a); // set dir to the elevation angle.
+            dist += height / Mathf.Tan(a); // Correction for small height differences
+
+            // Calculate the velocity magnitude
+            float velocity = Mathf.Sqrt(dist * Physics.gravity.magnitude / Mathf.Sin(2 * a));
+            return velocity * dir.normalized; // Return a normalized vector.
+        }
         // The indices in the container that contains the hands
         private int[] handSlots;
         private Inventory inventory;
